11dd11b19658f499486d11769736163ecd940fa4
Alexandr1207 <andrianov-05@mail.ru>
2026-04-23 13:26:53 +0500

    Some changes in vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'movies_react_app',
+  base: '/movies_react_app/',
   plugins: [react()],
 })
